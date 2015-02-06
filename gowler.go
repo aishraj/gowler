940ada31a9d7b15419d96ba15ea78e0584060ca5
Aishraj <aishraj@users.noreply.github.com>
2015-02-06 07:34:36 +0530

    Adding few minor changes as per wikipedia

--- gowler.go
+++ gowler.go
@@ -6,8 +6,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
+//TODO: accept a set of beginUrls instead
 func Gowler(beginUrl string, crawlDelay int) (err error) {
 	log.SetPrefix("gowler.go   ")
 	urlChannel := make(chan string)
@@ -28,16 +30,31 @@ func sanitizeUrl(url string) (resolvedLinks string) {
 }
 
 func ScrapLinks(url string, urlChannel chan string) {
+	if !isUrlValid(url) {
+		return
+	}
 	log.Println("Scraping URL: ", url)
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	httpClient := &http.Client{Transport: tr}
-	resp, err := httpClient.Get(url)
+	//TODO: use one more channel and a go routine to filter out mime time text/html only.
+	req, err := http.NewRequest("GET", url, nil)
+
 	if err != nil {
 		log.Println("Unable to perform get on the url %v", url)
+		log.Fatal(err)
 		os.Exit(1)
 	}
+
+	req.Header.Set("User-Agent", "Golang Spider - Gowler 0.0.1")
+
+	resp, err := httpClient.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	defer resp.Body.Close()
 	hyperLinks := AggregateAnchorLinks(resp.Body)
 	for _, link := range hyperLinks {
@@ -45,4 +62,11 @@ func ScrapLinks(url string, urlChannel chan string) {
 			urlChannel <- link
 		}()
 	}
+}
+
+func isUrlValid(url string) bool {
+	if strings.Contains(url, "?") {
+		return false
+	}
+	return true
 }
