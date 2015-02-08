ea1b88071e1c356ae3e0d81f61be526e2891fd48
Aishraj <aishraj@users.noreply.github.com>
2015-02-08 12:34:27 +0530

    Changes around URL validation and using multiple seed urls

--- gowler.go
+++ gowler.go
@@ -2,47 +2,61 @@ package gowler
 
 import (
 	"crypto/tls"
+	"errors"
 	"github.com/PuerkitoBio/purell"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 )
 
 //TODO: accept a set of beginUrls instead
-func Gowler(beginUrl string, crawlDelay int) (err error) {
-	log.SetPrefix("gowler.go   ")
+func Gowler(beginUrls []string, crawlDelay int) (err error) {
+	log.SetPrefix("gowler.go ")
 	urlChannel := make(chan string)
 	go func() {
-		urlChannel <- beginUrl
+		for _, urlString := range beginUrls {
+			urlChannel <- urlString
+		}
 	}()
-	for url := range urlChannel {
-		url = sanitizeUrl(url)
-		go ScrapLinks(url, urlChannel)
+	for UrlString := range urlChannel {
+		UrlString = sanitizeUrl(UrlString)
+		if len(UrlString) == 0 {
+			log.Println("URL %s was not sanitized. Skipping.", UrlString)
+			continue
+		}
+		go ScrapLinks(UrlString, urlChannel)
 	}
 	return nil
 }
 
-func sanitizeUrl(url string) (resolvedLinks string) {
+func sanitizeUrl(UrlString string) (resolvedLinks string) {
 	//Purell looks like the only library which does this efficiently.
-	resolvedLinks = purell.MustNormalizeURLString(url, purell.FlagsUsuallySafeGreedy)
+
+	resolvedLinks = purell.MustNormalizeURLString(UrlString, purell.FlagsUsuallySafeGreedy)
+	/*
+		if err != nil {
+			log.Println(err)
+		}*/
 	return resolvedLinks
 }
 
-func ScrapLinks(url string, urlChannel chan string) {
-	if !isUrlValid(url) {
+func ScrapLinks(UrlString string, urlChannel chan string) {
+	UrlString, err := validateUrl(UrlString)
+	if err != nil {
+		log.Println("The given urn %s is invalid.", UrlString)
 		return
 	}
-	log.Println("Scraping URL: ", url)
+	log.Println("Scraping URL: ", UrlString)
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	httpClient := &http.Client{Transport: tr}
-	//TODO: use one more channel and a go routine to filter out mime time text/html only.
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", UrlString, nil)
 
 	if err != nil {
-		log.Println("Unable to perform get on the url %v", url)
+		log.Println("Unable to perform get on the url %v", UrlString)
 		log.Fatal(err)
 		os.Exit(1)
 	}
@@ -57,6 +71,15 @@ func ScrapLinks(url string, urlChannel chan string) {
 
 	defer resp.Body.Close()
 	hyperLinks := AggregateAnchorLinks(resp.Body)
+
+	//TODO: use one more channel and a go routine to filter out mime time text/html only.
+	//The other channel will contain the actual URl list that has to be used.
+	// The go routine called will send a head request, and examine the content type header
+	// Only if the contenty type is text/html we add it to the actual frontier.
+
+	// Now regarding the frontier :
+	// The frontier should be : - asynchrounus, should have a logic for ageing.
+	//lets use a domain based re-visit policy, with aging for now.
 	for _, link := range hyperLinks {
 		go func() {
 			urlChannel <- link
@@ -64,9 +87,18 @@ func ScrapLinks(url string, urlChannel chan string) {
 	}
 }
 
-func isUrlValid(url string) bool {
-	if strings.Contains(url, "?") {
-		return false
+//TODO: This is very rudimentary. Will need better TLS/HTTPS support.
+func validateUrl(urlStr string) (string, error) {
+	if strings.Contains(urlStr, "?") {
+		return "", errors.New("Url contains a query param")
+	} else {
+		u, err := url.Parse(urlStr)
+		if err != nil {
+			log.Panic(err)
+		}
+		if u.Scheme == "" {
+			u.Scheme = "http"
+		}
+		return u.String(), nil
 	}
-	return true
 }
--- gowler_test.go
+++ gowler_test.go
@@ -4,9 +4,11 @@ import "testing"
 import "log"
 
 func TestGowler(t *testing.T) {
-	const testUrl = "http://aishraj.com"
-	err := Gowler(testUrl, 3)
+	const testUrl1 = "http://aishraj.com"
+	const testUrl2 = "https://en.wikipedia.org"
+	urlList := []string{testUrl1, testUrl2}
+	err := Gowler(urlList, 3)
 	if err != nil {
-		log.Println("Unable to start crawling on the site %v", testUrl)
+		log.Println("Unable to start crawling on the site %v", urlList)
 	}
 }
